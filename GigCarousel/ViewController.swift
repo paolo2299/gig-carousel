4cd5e2df24b938b002e8c9d9ab06dbb76244f16a
= <=>
2014-09-13 08:32:39 +0100

    Initial Commit

---
+++ GigCarousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GigCarousel
+//
+//  Created by Paul Lawson on 13/09/2014.
+//  Copyright (c) 2014 Paul Lawson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GigCarouselTests/GigCarouselTests.swift
@@ -0,0 +1,36 @@
+//
+//  GigCarouselTests.swift
+//  GigCarouselTests
+//
+//  Created by Paul Lawson on 13/09/2014.
+//  Copyright (c) 2014 Paul Lawson. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GigCarouselTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
